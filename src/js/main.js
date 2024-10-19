f1836d821330f6f29e706cb062f38363550d9ac8
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2024-10-19 22:55:46 +0700

    update lan 2

--- src/js/main.js
+++ src/js/main.js
@@ -47,3 +47,43 @@ const ketQua = () =>{
     }
     document.querySelector("#ketQua").innerHTML = "Tổng điểm: " + tongDiem + "<br> Kết quả: " + ketQua;
 };
+
+/**
+ * TÍNH TIỀN ĐIỆN
+ */
+const tienDien = () => {
+    console.log("test");
+    let ten = document.querySelector("#nhapTen").value;
+    let soKW = +document.querySelector("#nhapSoKW").value;
+
+    let tongTien = 0;
+    if (soKW == 50){
+        tongTien = 50 * 500;
+    }
+    else if (soKW <= 100){
+        tongTien = 50 * 500 + (soKW -50) * 650;
+    }
+    else if (soKW <=200){
+        tongTien = 50 * 500 + 50 * 650 + (soKW-100)*850;
+    }
+    else if (soKW <= 250){
+        tongTien = 50 * 500 + 50 * 650 + 100 * 850 + (soKW - 200)*1100;
+    }
+    else{
+        tongTien = 50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + (soKW-350)*1300;
+    }
+    document.querySelector("#ketQuaTienDien").innerHTML = `Tên: ${ten} <br> Tổng tiền điện: ${tongTien}`;
+};
+
+/**
+ * TÍNH THUẾ THU NHẬP CÁ NHÂN
+ */
+const thueCaNhan = () => {
+    console.log("test");
+    let hoTen = document.querySelector("#hoTen").value;
+    let thuNhapCaNam = +document.querySelector("#thuNhapNam").value;
+    let soNguoiPhuThuoc = +document.querySelector("#phuThuoc").value;
+
+    let thuNhapChiuThue = 0;
+    let thuNhapCaNhan = 0;
+}
